1fdd8f92a04b976b334aca7bba4d3b665ba0fe4c
Tanner555 <theman989898@gmail.com>
2018-08-30 05:05:03 -0700

    Added OrthosizeEditorUtility Script

---
+++ SmallGameForJamTest/Assets/SmallGameProject/Scripts/Utilities/OrthosizeEditorUtility.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LazarusClone
+{
+    [ExecuteInEditMode]
+    public class OrthosizeEditorUtility : MonoBehaviour
+    {
+        #region FieldsAndProps
+        public float desiredCameraHeight = 5f;
+        private float repeatRate = 0.05f;
+
+        Camera myCamera
+        {
+            get
+            {
+                if (_myCamera == null)
+                    _myCamera = Camera.main;
+
+                return _myCamera;
+            }
+        }
+        Camera _myCamera = null;
+
+        float desiredAspect
+        {
+            get { return 16f / 9f; }
+        }
+
+        float aspect
+        {
+            get { return myCamera.aspect; }
+        }
+
+        float ratio
+        {
+            get { return desiredAspect / aspect; }
+        }
+        #endregion
+
+        private void Update()
+        {
+            UpdateOrthographicCameraHeight();
+        }
+
+        void UpdateOrthographicCameraHeight()
+        {
+            if (myCamera == null ||
+                myCamera.orthographic == false)
+            {
+                if (myCamera == null)
+                    Debug.Log("myCamera is null");
+
+                if (myCamera.orthographic == false)
+                    Debug.Log("myCamera isn't orthographic");
+
+                CancelInvoke();
+                return;
+            }
+
+            myCamera.orthographicSize = desiredCameraHeight * ratio;
+        }
+    }
+}
