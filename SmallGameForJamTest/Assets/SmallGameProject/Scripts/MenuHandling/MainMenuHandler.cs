4da441ed6162ac0c05fcbafc85fb5d66cdbdcb90
Tanner555 <theman989898@gmail.com>
2018-08-30 00:57:39 -0700

    Added GameInstance and Level Handling Scripts

---
+++ SmallGameForJamTest/Assets/SmallGameProject/Scripts/MenuHandling/MainMenuHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LazarusClone
+{
+    /// <summary>
+    /// Temporary Class For Handling the Main Menu
+    /// Will Likely Replace in the Future with a Global
+    /// Game Manager Class
+    /// </summary>
+    public class MainMenuHandler : MonoBehaviour
+    {
+        GameInstance gameInstance
+        {
+            get { return GameInstance.thisInstance; }
+        }
+
+        [SerializeField]
+        public LevelIndex loadLevel;
+        [SerializeField]
+        public ScenarioIndex scenario;
+
+        public void Btn_PlayGame()
+        {
+            if (gameInstance != null)
+            {
+                gameInstance.LoadLevel(loadLevel, scenario);
+            }
+        }
+
+        public void Btn_QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
